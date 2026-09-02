b30870b4c010536927ab2e1cb72fef950647813f
Khanh Ny <nyhnk4900@ut.edu.vn>
2026-09-02 15:29:05 +0700

    Lưu thay đổi file Connect.php

--- Connect.php
+++ Connect.php
@@ -3,6 +3,6 @@
 define('ROOT_PATH', __DIR__);
 
 $link = mysqli_connect("db", "webuser", "webpass123", "db_LexiLoop");    die("Kết nối thất bại: " . mysqli_connect_error());
-}
+
 mysqli_set_charset($link, "utf8mb4");
 ?>
